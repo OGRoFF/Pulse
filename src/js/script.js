3c0db2201dfcc1fa30ccabde3ed156f6bcbf0878
OLEG <oleg,gromov77@gmail.com>
2021-01-27 22:24:49 +0300

    js0.5%

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+
+//number
+
+//string "", '', ``
+
+//true/false
+
+//null
+
+//undefined
+
+/* let obj = {
+    name: 'apple'
+    color: 'green'
+    weight: 200
+} */
+
+//symbol
+
+//alert (1234);
+//let number = 4;
+//console.log(number);
+/* let answer = prompt("Вам есть 18 лет?");
+console.log(answer); */
