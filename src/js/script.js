5e85474a26a399b680759023d4a6d7c1b2f86512
OLEG <oleg,gromov77@gmail.com>
2021-01-28 22:02:56 +0300

    js_

--- src/js/script.js
+++ src/js/script.js
@@ -22,3 +22,47 @@
 //console.log(number);
 /* let answer = prompt("Вам есть 18 лет?");
 console.log(answer); */
+
+/* let isChecked = true,
+    isClosed = false; */
+
+/* console.log(isChecked && isClosed);
+ */
+/* console.log(isChecked || isClosed);
+ */
+
+/* if (2*5 == 8*1) {
+    console.log('верное выражение')
+} else {
+    console.log('ошибка')
+} */
+
+/* let answer = confirm("Вам есть 18 лет?");
+if (answer) {
+    console.log('open')
+} else {
+    console.log('go away')
+}
+
+const num = 50;
+
+if (num<49){
+    console.log('wrong')
+} else if (num>100) {
+    console.log('many')
+} else {
+    console.log('ok')
+} */
+/* 
+for(let i = 1; i<8;i++) {
+    console.log(i)
+}
+ */
+
+function logging (a, b) {
+    console.log (a + b)
+}
+
+logging(3, 5);
+
+logging(6, 8);
