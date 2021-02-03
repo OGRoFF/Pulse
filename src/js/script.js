efe2eeeda40fde8688ff03be7cb93f24c2582f46
Oleg <oleg.gromov77@gmail.com>
2021-02-03 21:29:01 +0300

    slick_resp

--- src/js/script.js
+++ src/js/script.js
@@ -67,38 +67,27 @@ logging(3, 5);
 
 logging(6, 8); */
 
-/* $(document).ready(function(){
+$(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-      //  adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false
-              }
-            },
+                {
+                breakpoint: 992,
+                settings: {
+                    dots: false,
+                    arrows: false
+                }
+            }
+           /*  {
+                breakpoint: 600,
+                settings: {
+                    dots: false
+                }
+            } */
         ]
 
       });
   });
-       */
-
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false
-});
-
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-  });
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-  });
+      
